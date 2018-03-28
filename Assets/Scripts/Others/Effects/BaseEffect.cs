ecfa0a40fe3971aec4e4876fa0f5d37fee5dbc23
Joao Vitor <jneiva42@gmail.com>
2018-03-28 15:25:13 -0300

    base effects
    base

---
+++ Assets/Scripts/Others/Effects/BaseEffect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hexagon.Effects 
+{
+    public class BaseEffect : MonoBehaviour 
+    {
+
+        
+    }
+}
+
+
