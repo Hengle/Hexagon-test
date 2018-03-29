16d1bfd14854346896b2778fdeec08edcd48f30e
Joao Neiva <jneiva42@gmail.com>
2018-03-28 23:15:08 -0300

    Ability Effect pre

---
+++ Assets/Scripts/ECS/Engines/Ability/EffectApplySystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Svelto.ECS;
+using UnityEngine;
+
+namespace Hexagon.ECS.Ability
+{
+    public class EffectApplySystem : SingleEntityViewEngine<AbilityEntityView>
+    {
+
+        protected override void Add(AbilityEntityView entityView)
+        {
+            entityView.effectsComponent.apply.NotifyOnValueSet(OnApplyEffects);
+        }
+
+        protected override void Remove(AbilityEntityView entityView)
+        {
+            entityView.effectsComponent.apply.StopNotify(OnApplyEffects);
+        }
+
+        void OnApplyEffects(int senderId, bool apply)
+        {
+
+        }
+    }
+}
+
